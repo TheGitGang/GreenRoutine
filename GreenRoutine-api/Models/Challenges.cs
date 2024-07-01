8e557eecad3a9a7da1f64c9186567a0d42fbeade
sunsonni <sonniemnguyen@gmail.com>
2024-07-01 15:03:25 -0500

    adding category model and props to challenges

--- GreenRoutine-api/Models/Challenges.cs
+++ GreenRoutine-api/Models/Challenges.cs
@@ -1,6 +1,15 @@
-namespace GreenRoutine;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenRoutine.Models;
 
 public class Challenge
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Difficulty { get; set; }
+    public TimeSpan Length { get; set; }
+    public string Description { get; set; } 
+    Category Category { get; set; }
     
 }
