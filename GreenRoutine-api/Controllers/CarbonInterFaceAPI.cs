51aa04cab7746e391eac39ccd3db1c6cd75a9379
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-07-02 14:08:53 -0500

    Testing if things work

--- GreenRoutine-api/Controllers/CarbonInterFaceAPI.cs
+++ GreenRoutine-api/Controllers/CarbonInterFaceAPI.cs
@@ -6,8 +6,11 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 
 namespace TodoApi;
+
 
 public class CarbonInterFaceAPI
 {  
@@ -16,12 +19,24 @@ public class CarbonInterFaceAPI
         public string Vehicle_model { get; set; }
        public string  Vehicle_year { get; set; }
  
+    
     public CarbonInterFaceAPI (int id, string vehicle_make, string vehicle_model, string vehicle_year)
     {
         Id = id;
         Vehicle_make = vehicle_make;
         Vehicle_model = vehicle_model;
         Vehicle_year = vehicle_year;
+    Console.WriteLine(Vehicle_make);
+    Console.WriteLine(Vehicle_model);
+    Console.WriteLine(Vehicle_year);
+    Console.WriteLine(Id);
     }
 }
+
+      
+
+
+
+
+
 
