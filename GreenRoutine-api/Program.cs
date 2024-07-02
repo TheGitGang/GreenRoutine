b4179ba11b9aad9fe6734338cb7c74a442be7580
sunsonni <sonniemnguyen@gmail.com>
2024-07-01 21:52:33 -0500

    Fixing program file

--- GreenRoutine-api/Program.cs
+++ GreenRoutine-api/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ChallengeDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
