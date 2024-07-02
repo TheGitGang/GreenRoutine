a5d175b17101418f6751c143f1d12ebe3c6e6ed1
kevinbaranowski <72942742+kevinbaranowski@users.noreply.github.com>
2024-07-02 10:01:54 -0500

    more authentication

---
+++ GreenRoutine-api/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Server.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+    }
+}
