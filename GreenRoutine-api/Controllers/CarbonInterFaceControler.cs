3bd36fb5534a2e4df24a9bba95d541de53e85a2e
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-07-05 09:31:03 -0500

    just stuff

--- GreenRoutine-api/Controllers/CarbonInterFaceControler.cs
+++ GreenRoutine-api/Controllers/CarbonInterFaceControler.cs
@@ -1,57 +1,33 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using TodoApi.Models;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
+[Route("api/[controller]")]
+[ApiController]
+public class CarbonController : ControllerBase
+{
+    private readonly CarbonInterfaceClient _carbonClient;
 
-namespace TodoApi.Controllers;
- [ApiController]
-    [Route("[controller]")]
-
-public class CarbonInterFaceController : ControllerBase
-{  
-    
-private static readonly HttpClient client = new HttpClient();
-
-    public static async Task<string> GetCarbonDataAsync(string apiKey)
+    public CarbonController(CarbonInterfaceClient carbonClient)
     {
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        client.DefaultRequestHeaders.Add("Authorization", "Bearer 11oVkRMXcMSHMlUmyTKrg");
-
-        try
-        {
-            HttpResponseMessage response = await client.GetAsync("https://www.carboninterface.com/api/v1/vehicle_makes");
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-            return responseBody;
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine($"Error: {e.Message}");
-            return null;
-        }
+        _carbonClient = carbonClient;
     }
-        [HttpGet]
-        public async Task<IActionResult> Get()
+
+    [HttpPost("get-vehicle-emissions")]
+    public async Task<IActionResult> GetVehicleEmissions([FromBody] EmissionRequest request)
+    {
+        if (request.Type != "vehicle")
         {
-            string apiKey = "11oVkRMXcMSHMlUmyTKrg";
-            var result = await GetCarbonDataAsync(apiKey);
-            if (result != null)
-            {
-                return Ok(result);
-            }
-            return StatusCode(500, "An error occurred while fetching data");
+            return BadRequest("Invalid request type");
         }
+
+        var result = await _carbonClient.GetEmissionsAsync(request.Type, request.Data);
+        return Ok(result);
+    }
+
 }
 
+// make conditional logic for getting the dat you need 
+// git checkout development git pull 
 
 
 
---
+++ GreenRoutine-api/Controllers/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace TodoApi;
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+        services.AddSingleton(new CarbonInterfaceClient("11oVkRMXcMSHMlUmyTKrg"));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarbonInterfaceApp v1"));
+        }
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
