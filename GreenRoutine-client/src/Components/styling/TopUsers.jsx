1225d56090f50d67285a65d41ec6612d11ee7b3d
sunsonni <sonniemnguyen@gmail.com>
2024-07-12 15:09:16 -0500

    photos added

--- GreenRoutine-client/src/Components/styling/TopUsers.jsx
+++ GreenRoutine-client/src/Components/styling/TopUsers.jsx
@@ -1,8 +1,28 @@
 import './TopUsers.css';
 import { Col } from 'reactstrap';
+import { useState, useEffect } from 'react'
 
-const TopUser = ({ user, photo, rank }) => {
+
+const TopUser = ({ user, rank }) => {
+  const [userPhoto, setUserPhoto] = useState(null);
   const getClassName = () => {
+    const fetchUserPhoto = async () => {
+      const response = await fetch(`api/Account/${user.id}/getUserPhoto`, {
+        method: "GET"
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUserPhoto(data.photo)
+      } else {
+        setError('Unable to fetch user photo')
+      }
+    }
+    useEffect(() => {
+      if (user.id) {
+        fetchUserPhoto();
+      }
+    }, [user.id])
+  
     switch(rank) {
         case 0: 
             return 'First';
@@ -11,15 +31,15 @@ const TopUser = ({ user, photo, rank }) => {
         case 2:
             return 'Third';
         default:
-            return '';
-            
+            return '';    
     }
+
   }
 
     return (
       <Col className={`top-user ${getClassName()}`}>
         <h1>{getClassName(rank)}</h1>
-          <img src={photo} className='CircularImage'/>
+          <img src={userPhoto} className='CircularImage profile-photo'/>
             <h3>{user.userName}</h3>
             <h3>{user.leaves} Leaves</h3>
       </Col>
