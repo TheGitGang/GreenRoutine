db7774663c28063e19e21054bbd3d0c882550e95
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-07-29 19:26:34 -0500

    calendar completion colors put back

--- GreenRoutine-api/Controllers/UserChallengeController.cs
+++ GreenRoutine-api/Controllers/UserChallengeController.cs
@@ -60,13 +60,19 @@ public class UserChallengeController : ControllerBase
     [HttpGet("dates")]
      public async Task<ActionResult<IEnumerable<string>>> GetMarkedDates()
     {
-        Console.WriteLine("hi");
-        var markedDates = await context.UserChallenges.Select(c => c.SignupDate)
+        var greenDates = await context.UserChallenges.Where(c => c.ChallengeCompleted).Select(c => c.SignupDate)
                                         .Select(md => md.Date.ToString("yyyy-MM-dd"))
                                         .ToListAsync();
-
-        Console.WriteLine(markedDates.ToString());
-        return Ok(markedDates);
+        var yellowDates = await context.UserChallenges.Where(c => c.ChallengeCompleted == false).Select(c => c.SignupDate)
+                                        .Select(md => md.Date.ToString("yyyy-MM-dd"))
+                                        .ToListAsync();
+        var result = new
+        {
+            GreenDates = greenDates,
+            YellowDates = yellowDates
+        };
+        Console.WriteLine(result);
+        return Ok(result);
     }
     
 
